08edc5c26e7c53f87faa8534d63f8c9a6e2bd7a7
Jesse Richman <shadowarcher4@gmail.com>
2020-07-15 16:08:55 -0400

    Inital commit

---
+++ app.py
@@ -0,0 +1,135 @@
+import wx
+import app_gui
+from pyparrot.Bebop import Bebop
+import math
+import enum
+
+class CmdType(enum.Enum):
+    Direct="direct"
+    Relative="relative"
+    Sleep="sleep"
+    TakeOff="takeoff"
+    Land="land"
+    Flip="flip"
+
+class PyParrot(app_gui.MyFrame1):
+    def __init__(self, parent):
+        app_gui.MyFrame1.__init__(self, parent)
+        self.statusBar.SetStatusWidths([100, -1])
+        self.statusBar.SetStatusText('Not Connected')
+        self.lc_commands.InsertColumn(0, 'command', width=300)
+
+        self.bebop = Bebop()
+    
+    def OnConnect( self, event ):
+        state = self.bebop.connect(10)
+        statusText = 'Connected'
+        if not state:
+            statusText = 'Not ' + statusText
+        self.statusBar.SetStatusText(statusText)
+
+    def OnDisconnect( self, event ):
+        self.bebop.disconnect()
+        self.statusBar.SetStatusText('Disconnected')
+
+    def OnTakeOff( self, event ):
+        self.bebop.safe_takeoff(10)
+
+        # By default, setting the status text only sets the first
+        # one. So we must specify the second status text (index 1
+        # , instead of index 0)
+        self.statusBar.SetStatusText('Taking off', 1)
+
+    def OnLand( self, event ):
+        self.bebop.safe_land(10)
+        self.statusBar.SetStatusText('Landing', 1)
+
+    def OnAddTakeOff( self, event ):
+        self._addCommand(f'{CmdType.TakeOff.value},10')
+
+    def OnAddLand( self, event ):
+        self._addCommand(f'{CmdType.Land.value},10')
+
+    def OnAddRelative( self, event ):
+        x = int(self.fld_x.GetValue())
+        y = int(self.fld_y.GetValue())
+        z = int(self.fld_z.GetValue())
+        rads = int(self.fld_radians.GetValue())
+        self._addCommand(f'{CmdType.Relative.value},{x},{y},{z},{rads}')
+
+    def OnAddDirect( self, event ):
+        roll = int(self.fld_roll.GetValue())
+        pitch = int(self.fld_pitch.GetValue())
+        yaw = int(self.fld_yaw.GetValue())
+        vertical = int(self.fld_vertical.GetValue())
+        duration = int(self.fld_duration.GetValue())
+        self._addCommand(f'{CmdType.Direct.value},{roll},{pitch},{yaw},{vertical},{duration}')
+
+    def OnAddSleep( self, event ):
+        sleep = int(self.fld_sleep.GetValue())
+        self._addCommand(f'{CmdType.Sleep.value},{sleep}')
+
+    def OnAddFlip( self, event ):
+        dir = int(self.fld_direction)
+        self._addCommand(f'{CmdType.Flip.value},{dir}')
+
+    def OnRemove( self, event ):
+        self.lc_commands.DeleteItem(self.lc_commands.GetFocusedItem())
+
+    def OnUp( self, event ):
+        """Swap the selected item with the one above it"""
+        index = self.lc_commands.GetFocusedItem()
+        if index >= 1:
+            selItemStr = self.lc_commands.GetItemText(index)
+            aboveItemStr = self.lc_commands.GetItemText(index-1)
+            self.lc_commands.SetItemText(index, aboveItemStr)
+            self.lc_commands.SetItemText(index-1, selItemStr)
+            self.lc_commands.Focus(index-1)
+
+    def OnDown( self, event ):
+        """Swap the selected item with the one below it"""
+        index = self.lc_commands.GetFocusedItem()
+        if index < self.lc_commands.GetItemCount() - 1:
+            selItemStr = self.lc_commands.GetItemText(index)
+            belowItemStr = self.lc_commands.GetItemText(index+1)
+            self.lc_commands.SetItemText(index, belowItemStr)
+            self.lc_commands.SetItemText(index+1, selItemStr)
+            self.lc_commands.Focus(index+1)
+
+    def OnClear( self, event ):
+        self.lc_commands.DeleteAllItems()
+
+    def OnRunCommands( self, event ):
+        """Go through each item in lc_commands and convert the string to 
+        a list. Then use the first item in the list to determine the 
+        command type, and the rest of the items are the params"""
+        for i in range(self.lc_commands.GetItemCount()):
+            args = self.lc_commands.GetItemText(i).split(',')
+            self.statusBar.SetStatusText(f'Executing command: {args}', 1)
+
+            if (args[0] == CmdType.Sleep.value):
+                self.bebop.smart_sleep(int(args[1]))
+
+            elif args[0] == CmdType.TakeOff.value:
+                self.bebop.safe_takeoff(int(args[1]))
+
+            elif args[0] == CmdType.Land.value:
+                self.bebop.safe_land(int(args[1]))
+
+            elif args[0] == CmdType.Direct.value:
+                self.bebop.fly_direct(int(args[1]), int(args[2]), int(args[3]), int(args[4]), int(args[5]))
+
+            elif args[0] == CmdType.Relative.value:
+                self.bebop.move_relative(int(args[1]), int(args[2]), int(args[3]), math.radians(int(args[4])))
+
+            elif args[0] == CmdType.Flip.value:
+                self.bebop.flip(int(args[1]))
+    
+    def _addCommand(self, cmd: str):
+        self.lc_commands.InsertItem(self.lc_commands.GetItemCount(), cmd)
+        self.statusBar.SetStatusText(f'Added command: {cmd}', 1)
+# end of class
+
+application = wx.App(False) # create new application
+PyParrot(None).Show(True) # build and show our frame
+application.MainLoop() # run the application
---
+++ app_gui.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Oct 26 2018)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class MyFrame1
+###########################################################################
+
+class MyFrame1 ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 726,591 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+		bSizer3 = wx.BoxSizer( wx.VERTICAL )
+
+		bSizer4 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.btn_connect = wx.Button( self, wx.ID_ANY, u"Connect", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.btn_connect, 0, wx.ALL, 5 )
+
+		self.btn_disconnect = wx.Button( self, wx.ID_ANY, u"Disconnect", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.btn_disconnect, 0, wx.ALL, 5 )
+
+		self.btn_takeOff = wx.Button( self, wx.ID_ANY, u"Take Off", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.btn_takeOff, 0, wx.ALL, 5 )
+
+		self.btn_land = wx.Button( self, wx.ID_ANY, u"Land", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.btn_land, 0, wx.ALL, 5 )
+
+
+		bSizer3.Add( bSizer4, 0, wx.EXPAND, 5 )
+
+		self.m_notebook2 = wx.Notebook( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_panel6 = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer131 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.btn_addTakeOff = wx.Button( self.m_panel6, wx.ID_ANY, u"Add Take Off", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer131.Add( self.btn_addTakeOff, 0, wx.ALL, 5 )
+
+		self.btn_addLand = wx.Button( self.m_panel6, wx.ID_ANY, u"Add Land", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer131.Add( self.btn_addLand, 0, wx.ALL, 5 )
+
+
+		self.m_panel6.SetSizer( bSizer131 )
+		self.m_panel6.Layout()
+		bSizer131.Fit( self.m_panel6 )
+		self.m_notebook2.AddPage( self.m_panel6, u"Take Off/Land", False )
+		self.m_panel2 = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer5 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.fld_x = wx.TextCtrl( self.m_panel2, wx.ID_ANY, u"x", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.fld_x, 1, wx.ALL, 5 )
+
+		self.fld_y = wx.TextCtrl( self.m_panel2, wx.ID_ANY, u"y", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.fld_y, 1, wx.ALL, 5 )
+
+		self.fld_z = wx.TextCtrl( self.m_panel2, wx.ID_ANY, u"z", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.fld_z, 1, wx.ALL, 5 )
+
+		self.fld_radians = wx.TextCtrl( self.m_panel2, wx.ID_ANY, u"radians", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.fld_radians, 1, wx.ALL, 5 )
+
+		self.btn_addRelative = wx.Button( self.m_panel2, wx.ID_ANY, u"Add", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.btn_addRelative, 0, wx.ALL, 5 )
+
+
+		self.m_panel2.SetSizer( bSizer5 )
+		self.m_panel2.Layout()
+		bSizer5.Fit( self.m_panel2 )
+		self.m_notebook2.AddPage( self.m_panel2, u"Fly Relative", False )
+		self.m_panel3 = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer6 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.fld_roll = wx.TextCtrl( self.m_panel3, wx.ID_ANY, u"roll", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.fld_roll, 1, wx.ALL, 5 )
+
+		self.fld_pitch = wx.TextCtrl( self.m_panel3, wx.ID_ANY, u"pitch", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.fld_pitch, 1, wx.ALL, 5 )
+
+		self.fld_yaw = wx.TextCtrl( self.m_panel3, wx.ID_ANY, u"yaw", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.fld_yaw, 1, wx.ALL, 5 )
+
+		self.fld_vertical = wx.TextCtrl( self.m_panel3, wx.ID_ANY, u"vertical", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.fld_vertical, 1, wx.ALL, 5 )
+
+		self.fld_duration = wx.TextCtrl( self.m_panel3, wx.ID_ANY, u"duration", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.fld_duration, 1, wx.ALL, 5 )
+
+		self.btn_addDirect = wx.Button( self.m_panel3, wx.ID_ANY, u"Add", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.btn_addDirect, 0, wx.ALL, 5 )
+
+
+		self.m_panel3.SetSizer( bSizer6 )
+		self.m_panel3.Layout()
+		bSizer6.Fit( self.m_panel3 )
+		self.m_notebook2.AddPage( self.m_panel3, u"Fly Direct", False )
+		self.m_panel4 = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer15 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.fld_sleep = wx.TextCtrl( self.m_panel4, wx.ID_ANY, u"sleep", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer15.Add( self.fld_sleep, 1, wx.ALL, 5 )
+
+		self.btn_addSleep = wx.Button( self.m_panel4, wx.ID_ANY, u"Add", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer15.Add( self.btn_addSleep, 0, wx.ALL, 5 )
+
+
+		self.m_panel4.SetSizer( bSizer15 )
+		self.m_panel4.Layout()
+		bSizer15.Fit( self.m_panel4 )
+		self.m_notebook2.AddPage( self.m_panel4, u"Sleep", True )
+		self.m_panel5 = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer12 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.fld_direction = wx.TextCtrl( self.m_panel5, wx.ID_ANY, u"direction", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer12.Add( self.fld_direction, 1, wx.ALL, 5 )
+
+		self.btn_addFlip = wx.Button( self.m_panel5, wx.ID_ANY, u"Add", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer12.Add( self.btn_addFlip, 0, wx.ALL, 5 )
+
+
+		self.m_panel5.SetSizer( bSizer12 )
+		self.m_panel5.Layout()
+		bSizer12.Fit( self.m_panel5 )
+		self.m_notebook2.AddPage( self.m_panel5, u"Flip", False )
+
+		bSizer3.Add( self.m_notebook2, 1, wx.EXPAND |wx.ALL, 5 )
+
+		bSizer14 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.lc_commands = wx.ListCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LC_NO_HEADER|wx.LC_REPORT|wx.LC_SINGLE_SEL )
+		bSizer14.Add( self.lc_commands, 1, wx.ALL|wx.EXPAND, 5 )
+
+		bSizer8 = wx.BoxSizer( wx.VERTICAL )
+
+		self.btn_remove = wx.Button( self, wx.ID_ANY, u"Remove", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer8.Add( self.btn_remove, 0, wx.ALL|wx.EXPAND, 5 )
+
+		self.btn_up = wx.Button( self, wx.ID_ANY, u"Up", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer8.Add( self.btn_up, 0, wx.ALL|wx.EXPAND, 5 )
+
+		self.btn_down = wx.Button( self, wx.ID_ANY, u"Down", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer8.Add( self.btn_down, 0, wx.ALL|wx.EXPAND, 5 )
+
+		self.btn_clear = wx.Button( self, wx.ID_ANY, u"Clear", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer8.Add( self.btn_clear, 0, wx.ALL|wx.EXPAND, 5 )
+
+
+		bSizer14.Add( bSizer8, 0, wx.EXPAND, 5 )
+
+
+		bSizer3.Add( bSizer14, 1, wx.EXPAND, 5 )
+
+		self.btn_runCommands = wx.Button( self, wx.ID_ANY, u"Run Commands", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer3.Add( self.btn_runCommands, 0, wx.ALL|wx.EXPAND, 5 )
+
+
+		self.SetSizer( bSizer3 )
+		self.Layout()
+		self.statusBar = self.CreateStatusBar( 2, wx.STB_SIZEGRIP, wx.ID_ANY )
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.btn_connect.Bind( wx.EVT_BUTTON, self.OnConnect )
+		self.btn_disconnect.Bind( wx.EVT_BUTTON, self.OnDisconnect )
+		self.btn_takeOff.Bind( wx.EVT_BUTTON, self.OnTakeOff )
+		self.btn_land.Bind( wx.EVT_BUTTON, self.OnLand )
+		self.btn_addTakeOff.Bind( wx.EVT_BUTTON, self.OnAddTakeOff )
+		self.btn_addLand.Bind( wx.EVT_BUTTON, self.OnAddLand )
+		self.btn_addRelative.Bind( wx.EVT_BUTTON, self.OnAddRelative )
+		self.btn_addDirect.Bind( wx.EVT_BUTTON, self.OnAddDirect )
+		self.btn_addSleep.Bind( wx.EVT_BUTTON, self.OnAddSleep )
+		self.btn_addFlip.Bind( wx.EVT_BUTTON, self.OnAddFlip )
+		self.btn_remove.Bind( wx.EVT_BUTTON, self.OnRemove )
+		self.btn_up.Bind( wx.EVT_BUTTON, self.OnUp )
+		self.btn_down.Bind( wx.EVT_BUTTON, self.OnDown )
+		self.btn_clear.Bind( wx.EVT_BUTTON, self.OnClear )
+		self.btn_runCommands.Bind( wx.EVT_BUTTON, self.OnRunCommands )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, overide them in your derived class
+	def OnConnect( self, event ):
+		event.Skip()
+
+	def OnDisconnect( self, event ):
+		event.Skip()
+
+	def OnTakeOff( self, event ):
+		event.Skip()
+
+	def OnLand( self, event ):
+		event.Skip()
+
+	def OnAddTakeOff( self, event ):
+		event.Skip()
+
+	def OnAddLand( self, event ):
+		event.Skip()
+
+	def OnAddRelative( self, event ):
+		event.Skip()
+
+	def OnAddDirect( self, event ):
+		event.Skip()
+
+	def OnAddSleep( self, event ):
+		event.Skip()
+
+	def OnAddFlip( self, event ):
+		event.Skip()
+
+	def OnRemove( self, event ):
+		event.Skip()
+
+	def OnUp( self, event ):
+		event.Skip()
+
+	def OnDown( self, event ):
+		event.Skip()
+
+	def OnClear( self, event ):
+		event.Skip()
+
+	def OnRunCommands( self, event ):
+		event.Skip()
+
+
